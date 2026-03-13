ad176eed2183e1c60a544450bd23c65f99316a97
Afonso de Mori <me@afonso.dev>
2026-03-13 22:35:31 +0000

    ci: Add build for .deb, .rpm, and .apk; and push to afonsodemori/packages

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-# fns-cli
-brew remove fns-cli || true
-make dev
-fns-cli config import-extras
+# fns-cli -- defensive, with "|| true" in case dev repo is failling state
+make dev || true
+fns-cli config import-extras || true
 grep -qxF 'source ~/fns-cli.bash_profile' "$HOME/.bashrc" || printf '\nsource ~/fns-cli.bash_profile\n' >>"$HOME/.bashrc"
 
 # Ensure gemini directory is writable
